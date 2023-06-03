4df0f902a5a3c7a6266b158f51d3af3f176aab7f
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2023-06-03 17:24:38 -0300

    added navbar and avatar responsive

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
 import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import { Menu, MenuItem, Sidebar, SubMenu, useProSidebar } from 'react-pro-sidebar';
 import Login from './pages/Login/Login'
@@ -19,20 +20,59 @@ import { useLocalStorage } from './hooks/useLocalStorage';
 import { StorageKeys } from './helpers/StorageKeys';
 
 function App() {
-  const { collapseSidebar } = useProSidebar();
-
+  const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } = useProSidebar();
   const {setBodyColor} = useSetBodyColor()
   const {getItem, setItem} = useLocalStorage()
   const [logged, setLogged] = useState(false)
+  const [avatarH, setAvatarH] = useState('200px')
 
   useEffect(()=>{
+    toggle()
+    minimizeAvatar()
     setBodyColor("#ffffff")
-    setItem(StorageKeys.ACCESS_TOKEN, '')
+    document.documentElement.style.setProperty('--avatarH', avatarH)
     const token: any = getItem(StorageKeys.ACCESS_TOKEN);
     console.log(token)
     token ? setLogged(true) : setLogged(false)
     console.log(logged)
   },[])
+
+  const toggle = () => {
+    toggleSidebar();
+    if (toggled) {
+      console.log(toggled)
+      collapseSidebar();
+      minimizeAvatar();
+    } else {
+      console.log(toggled)
+      collapseSidebar();
+      maximizeAvatar();
+    }
+  };
+
+  const exit = () =>{
+    setItem(StorageKeys.ACCESS_TOKEN, '')
+    window.location.reload()
+  }
+
+  const minimizeAvatar = () =>{
+    console.log('min')
+    setAvatarH('60px')
+    document.documentElement.style.setProperty('--avatarH', avatarH)
+  }
+
+  const maximizeAvatar = () =>{
+    console.log('max')
+    setAvatarH('200px')
+    document.documentElement.style.setProperty('--avatarH', avatarH)
+  }
+
+
+
+  // const showSidebar = () => {
+  //   setDisplayType('block')
+  //   collapseSidebar();
+  // }
 
   // const logged = false
 
@@ -41,29 +81,30 @@ function App() {
     {logged ? (
       <div id="app">
         <BrowserRouter>
-          <Sidebar className='sidebar'>
+          <Sidebar className='sidebar' backgroundColor='' breakPoint="sm">
             <Menu>
-              <MenuItem icon={<MenuOutlinedIcon />} onClick={() => {collapseSidebar();}}style={{ textAlign: "center" }}>
-                {" "}
-                {/* <h2>Admin</h2> */}
-              </MenuItem>
-              <Link to={'/home'} className='link'><MenuItem className='icon' icon={<HomeOutlinedIcon />}>Home</MenuItem></Link>
-              <Link to={'/about'} className='link'><MenuItem icon={<PeopleOutlinedIcon />}>About</MenuItem></Link>
-              <Link to={'/projects'} className='link'><MenuItem icon={<ContactsOutlinedIcon />}>Projects</MenuItem></Link>
-              {/* <SubMenu icon={<HomeOutlinedIcon />} label="SubMenu">
-                  <MenuItem icon={<PeopleOutlinedIcon />}>Item 1</MenuItem>
-                  <MenuItem icon={<PeopleOutlinedIcon />}>Item 2</MenuItem>
-                  <MenuItem icon={<PeopleOutlinedIcon />}>Item 3</MenuItem>
-                </SubMenu>
-              <MenuItem icon={<ContactsOutlinedIcon />}>Contacts</MenuItem>
-              <MenuItem icon={<ReceiptOutlinedIcon />}>Profile</MenuItem>
-              <MenuItem icon={<HelpOutlineOutlinedIcon />}>FAQ</MenuItem>
-              <MenuItem icon={<CalendarTodayOutlinedIcon />}>Calendar</MenuItem> */}
+              <MenuItem className='collapse-btn' icon={<MenuOutlinedIcon/>} onClick={() => {toggle();}}></MenuItem>
+              <div className='avatar'>
+              </div>
+              <Link to={'/home'} className='link'><MenuItem onClick={()=>{ broken && toggle()}} className='icon' icon={<HomeOutlinedIcon />}>Home</MenuItem></Link>
+              <Link to={'/about'} className='link'><MenuItem onClick={()=>{ broken && toggle()}} icon={<PeopleOutlinedIcon />}>About</MenuItem></Link>
+              <Link to={'/projects'} className='link'><MenuItem onClick={()=>{ broken && toggle()}} icon={<ContactsOutlinedIcon />}>Projects</MenuItem></Link>
+              <Link to={'/login'} className='link'><MenuItem onClick={exit} icon={<ExitToAppIcon />}>Exit</MenuItem></Link>
             </Menu>
           </Sidebar>
+          <div className="navbar">
+            {broken &&
+                <div className="icon-sidebar" onClick={()=>{toggle()}}>
+                 <MenuOutlinedIcon className='position-absolute top-0 start-0'/>
+              </div>
+            }
+            <div>
+              <ExitToAppIcon onClick={exit} className='position-absolute top-0 end-0'/>
+            </div>
+          </div>
           <Routes>
           <Route path='/home' Component={Home}></Route>
-          <Route path='/' Component={Home}></Route>
+          <Route path='/login' Component={Home}></Route>
           <Route path='/about' Component={About}></Route>
           <Route path='/projects' Component={Projects}></Route>
         </Routes>
