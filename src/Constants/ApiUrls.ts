5fd5a2c01cc8b04f26efb4580d35c5aaafb06220
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2023-12-14 20:23:00 -0300

    feat: update login url

--- src/Constants/ApiUrls.ts
+++ src/Constants/ApiUrls.ts
@@ -1,4 +1,4 @@
 export enum ApiUrls {
   // LOGIN = 'https://portfolio-api-g4lo.onrender.com/api/v1/auth/login'
-  LOGIN = 'https://portfolioapi-toffnxjumq-rj.a.run.app/api/v1/auth/login'
+  LOGIN = 'https://deploy-portfolio-api-okynoi3nca-uc.a.run.app/api/v1/auth/login'
 }
